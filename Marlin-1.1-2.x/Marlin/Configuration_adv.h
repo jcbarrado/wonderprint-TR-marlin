25b109a2db41aba075c7684b1f388e28ef2920e1
Juan Carlos Barrado <juan.carlos.barrado@accenture.com>
2019-06-11 09:51:27 +0200

    Corrección en la aceleración y amperaje para el eje Z.

--- Marlin-1.1-2.x/Marlin/Configuration_adv.h
+++ Marlin-1.1-2.x/Marlin/Configuration_adv.h
@@ -1102,13 +1102,13 @@
   #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
   #define INTERPOLATE       false  // Interpolate X/Y/Z_MICROSTEPS to 256
 
-  #define X_CURRENT          850  // rms current in mA. Multiply by 1.41 for peak current.
+  #define X_CURRENT          800  // rms current in mA. Multiply by 1.41 for peak current.
   #define X_MICROSTEPS        16  // 0..256
 
-  #define Y_CURRENT          850
+  #define Y_CURRENT          800
   #define Y_MICROSTEPS        16
 
-  #define Z_CURRENT         1185
+  #define Z_CURRENT          900
   #define Z_MICROSTEPS        16
 
   #define X2_CURRENT         800
@@ -1120,7 +1120,7 @@
   #define Z2_CURRENT         800
   #define Z2_MICROSTEPS       16
 
-  #define E0_CURRENT         850
+  #define E0_CURRENT         800
   #define E0_MICROSTEPS       16
 
   #define E1_CURRENT         800
